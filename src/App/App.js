75b7532774dd9c9ddd7bf588ae183ec8b3c5fa1e
Jose Marquez <josesinmarquezpalmeros@gmail.com>
2020-05-30 13:21:21 -0500

    added api keys

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
 import './App.scss';
 
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
   render() {
     return (
       <div className="App">
