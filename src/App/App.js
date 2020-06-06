80c781afc02c6268b4c94a1894ca5ecbb548505d
Jose Marquez <josesinmarquezpalmeros@gmail.com>
2020-06-06 13:11:38 -0500

    got some installs and the navbar to work

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,38 @@
 import React from 'react';
-
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
-import fbConnection from '../helpers/data/connection';
+import {
+  BrowserRouter,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
 
-import Auth from '../components/Auth/Auth';
-import MyNavbar from '../components/MyNavbar/MyNavbar';
+import MyNavbar from '../components/shared/MyNavbar/MyNavbar';
+
+import Auth from '../components/pages/Auth/Auth';
+import Home from '../components/pages/Home/Home';
 
 import './App.scss';
 
+import fbConnection from '../helpers/data/connection';
+
 fbConnection();
+
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === false
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/home', state: { from: props.location } }} />));
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === true
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/auth', state: { from: props.location } }} />));
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
 
 class App extends React.Component {
   state = {
@@ -33,20 +55,23 @@ class App extends React.Component {
 
   render() {
     const { authed } = this.state;
-    const loadComponent = () => {
-      let componentToLoad = '';
-      if (authed) {
-        componentToLoad = <h1>Main body</h1>;
-      } else {
-        componentToLoad = <Auth/>;
-      }
-      return componentToLoad;
-    };
 
     return (
       <div className="App">
-        <MyNavbar authed={authed}/>
-        {loadComponent()}
+        <BrowserRouter>
+          <React.Fragment>
+            <MyNavbar authed={authed}/>
+            <div className="container">
+              <div className="row">
+                <Switch>
+                  <PrivateRoute path='/home'component={Home} authed={authed} />
+                  <PublicRoute path='/auth' component={Auth} authed={authed} />
+                  <Redirect from="*" to="/home"/>
+                </Switch>
+              </div>
+            </div>
+          </React.Fragment>
+        </BrowserRouter>
       </div>
     );
   }
