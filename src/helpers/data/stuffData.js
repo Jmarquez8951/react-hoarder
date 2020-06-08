fe8d2141442c54ca212402c590ea0661d46e8090
Jose Marquez <josesinmarquezpalmeros@gmail.com>
2020-06-07 19:15:37 -0500

    made the get requests

---
+++ src/helpers/data/stuffData.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import firebaseConfig from '../apiKeys.json';
+
+const baseUrl = firebaseConfig.firebaseKeys.databaseURL;
+
+const getStuffByUid = (uid) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/hoard.json`)
+    .then((response) => {
+      const fbStuff = response.data;
+      const stuff = [];
+      if (fbStuff) {
+        Object.keys(fbStuff).forEach((fbId) => {
+          fbStuff[fbId].id = fbId;
+          stuff.push(fbStuff[fbId]);
+        });
+      }
+      resolve(stuff);
+    })
+    .catch((err) => reject(err));
+});
+
+export default { getStuffByUid };
